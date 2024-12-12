651464b4febc66d272657b5b38da6e530cec064c
pravinshinde09 <138846741+pravinshinde09@users.noreply.github.com>
2024-12-12 12:44:35 +0530

    Added Search page Content.

--- app/screens/Search/Search.tsx
+++ app/screens/Search/Search.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
+import { StyleProps, useTheme } from '../../context/ThemeProvider'
+import Typography from '../../components/Typography';
 
 const Search = () => {
+  const { colors } = useTheme();
+  const styles = getStyles({ colors });
   return (
-    <View>
-      <Text>Search</Text>
+    <View style={styles.container}>
+      <Typography variant={'title03'}>Good things take time's</Typography>
+      <Typography variant={'title03'}>This feature is on its way!</Typography>
     </View>
   )
 }
 
 export default Search
 
-const styles = StyleSheet.create({})
+const getStyles = ({ colors }: StyleProps) => StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: colors.background,
+  },
+  message: {
+    fontSize: 18,
+    color: '#555',
+    textAlign: 'center',
+    paddingHorizontal: 20,
+  },
+})
